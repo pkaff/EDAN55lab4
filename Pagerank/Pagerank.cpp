307adbf711ccda7b46197474caea6855486a724d
tpi11nfa <tpi11nfa@student.lu.se>
2016-09-19 17:28:06 +0200

    Add files via upload

--- Pagerank/Pagerank.cpp
+++ Pagerank/Pagerank.cpp
@@ -2,10 +2,58 @@
 //
 
 #include "stdafx.h"
+#include <fstream>
+#include <vector>
+#include <iostream>
+#include <sstream>
 
+using namespace std;
+
+typedef vector<vector<size_t> > matrix;
+
+matrix adjacency_matrix(const string& filename) {
+	ifstream ifs(filename);
+	size_t nV;
+	string line = "";
+	getline(ifs, line);
+	istringstream is(line);
+	is >> nV;
+
+	matrix matrix(nV);
+	while (getline(ifs, line)) {
+		istringstream iss(line);
+		size_t u, v;
+		while (iss >> u >> v) {
+			matrix[u].push_back(v);
+		}
+	}
+
+
+	return matrix;
+}
+
+void print_matrix(const matrix& matrix) {
+	for (size_t r = 0; r < matrix.size(); ++r)
+	{
+		vector<size_t> row = matrix[r];
+		for (const size_t& i : row)
+		{
+			cout << r << " " << i << "\t";
+		}
+		cout << endl;
+	}
+	
+}
 
 int main()
 {
+	string filepath = "C:\\Users\\biu\\Documents\\Visual Studio 2015\\Projects\\PageRank\\Data\\";
+	string filename = "three.txt";
+
+	string full_path = filepath + filename;
+	matrix m = adjacency_matrix(full_path);
+
+	print_matrix(m);
     return 0;
 }
 
