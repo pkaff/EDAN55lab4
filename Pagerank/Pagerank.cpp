60ab67e96298ff775535616d8d0f40da011f08a1
Fawger <fagerberg.nils@gmail.com>
2016-09-20 18:58:17 +0200

    some logic added

--- Pagerank/Pagerank.cpp
+++ Pagerank/Pagerank.cpp
@@ -7,9 +7,17 @@
 #include <iostream>
 #include <sstream>
 
+#define METHOD_1
+//#define METHOD_2
+//#define METHOD_3
+
 using namespace std;
 
+//adjacency matrix
 typedef vector<vector<size_t> > matrix;
+
+//probability matrix
+typedef vector<vector<double> > P;
 
 matrix adjacency_matrix(const string& filename) {
 	ifstream ifs(filename);
@@ -27,8 +35,6 @@ matrix adjacency_matrix(const string& filename) {
 			matrix[u].push_back(v);
 		}
 	}
-
-
 	return matrix;
 }
 
@@ -42,7 +48,54 @@ void print_matrix(const matrix& matrix) {
 		}
 		cout << endl;
 	}
-	
+}
+
+void print_P(const P& P) {
+	for (const auto& row : P)
+	{
+		for (const double& i : row) { cout << i << " "; } cout << endl;
+	}
+}
+
+//get probability matrix from adjacency matrix
+P get_P(const matrix& m, double alpha = 0.85, bool sparse = true) {
+	size_t nV = m.size();
+	P P(nV);
+	for (auto col : m) {
+		size_t current = 0;
+		for (size_t i = 0; i < nV; ++i) {
+			int pr = 0;
+			if (col[i]) {
+
+			}
+		}
+	}
+	return P;
+}
+
+//approximate eigenvector
+vector<int> approx_eig(const P& P, vector<int> eig0 = vector<int>()) {
+	size_t nV = P.size();
+	if (eig0.size == 0) { eig0 = vector<int>(nV, 1); }
+
+//method 1: approximate eigenvector by p_i = p_(i-1)P, i = 1..m
+#ifdef METHOD_1
+
+#endif
+
+#ifdef METHOD_1
+
+#endif
+
+#ifdef METHOD_1
+
+#endif
+
+	return eig0;
+}
+
+void print_eig(vector<int> eig) {
+	for (const int& i : eig) { cout << i << " "; } cout << endl;
 }
 
 int main()
